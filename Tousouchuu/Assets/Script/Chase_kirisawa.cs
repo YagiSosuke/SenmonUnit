995e0213c6f73e43249885f58e79a6a1bc80b96f
krswytk <krswytk.0228@gmail.com>
2020-06-23 15:32:33 +0900

    追尾機能入りの●ねこをプレハブ生成

---
+++ Tousouchuu/Assets/Script/Chase_kirisawa.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.AI;
+
+public class Chase_kirisawa : MonoBehaviour
+{
+    public GameObject target;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.destination = target.transform.position;
+    }
+}
