0d907aec811ab8dcc325127b8c2894493bc8ea77
yagikun <yagikundayo@gmail.com>
2020-06-30 14:47:31 +0900

    クレジット画面の追加

---
+++ Tousouchuu/Assets/Script/CreditScene/GoTitleButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/*クレジット画面 - タイトルに戻るボタン処理*/
+
+public class GoTitleButton : MonoBehaviour
+{
+    //「タイトル」ボタンを押したら
+    public void TitleButtonPush()
+    {
+        SceneManager.LoadScene("Title");        //タイトル画面に遷移
+    }
+}
---
+++ Tousouchuu/Assets/Script/RetryGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RetryGame : MonoBehaviour
+{
+public void OnRetry()
+    {
+        SceneManager.LoadScene(
+            SceneManager.GetActiveScene().name);
+    }
+}
