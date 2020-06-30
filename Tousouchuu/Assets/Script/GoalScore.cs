4f897675f245efe43b424c66dcfba7c5a9657bc7
krswytk <krswytk.0228@gmail.com>
2020-06-30 14:17:50 +0900

    ゴール追加　及びテスト桐澤シーンをいじいじ

---
+++ Tousouchuu/Assets/Script/GoalScore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//作成者　桐澤　2020/6/30
+
+public class GoalScore : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        //門内部に接触したオブジェクトがタグ"Player"だった場合
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Score");//スコアシーンに移動させる
+            Debug.Log("ゴール接触");
+        }
+    }
+}
