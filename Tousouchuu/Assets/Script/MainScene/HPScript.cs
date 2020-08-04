c9f2615755138f5abc5a900896f41c695acf6cc1
yagikun <yagikundayo@gmail.com>
2020-08-04 14:40:43 +0900

    HPバーの追加（処理機能は無）

---
+++ Tousouchuu/Assets/Script/MainScene/HPScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*HPバーのスクリプト*/
+
+public class HPScript : MonoBehaviour
+{
+    public float HP = 1f;
+    [SerializeField] float Damage;
+    GameObject MainCamera;      //メインカメラのオブジェクト
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainCamera = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        gameObject.transform.LookAt(MainCamera.transform);
+    }
+}
