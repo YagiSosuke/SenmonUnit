116be9307adeaec5001d24073778439a8a90c780
yagikun <yagikundayo@gmail.com>
2020-08-11 15:06:29 +0900

    GUIのフォントを変更

---
+++ Tousouchuu/Assets/Script/MainSoundScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainSoundScript : MonoBehaviour{
+    public bool DontDestroyEnabled = true;
+
+    // Use this for initilization
+    void Start(){
+        if (DontDestroyEnabled) {
+            DontDestroyOnLoad(this);
+                }
+    }
+
+void LoadScene()
+    {
+        SceneManager.LoadScene("Tutorial1");
+    }
+}
